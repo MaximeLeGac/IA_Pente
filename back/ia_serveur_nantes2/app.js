5aa85bc4025c9df7c928ddb5c1910da837fd7ad4
AllanV <a.vanbleus@gmail.com>
2017-04-12 14:06:50 +0200

    Ajout des 2 IA

---
+++ back/ia_serveur_nantes2/app.js
@@ -0,0 +1,21 @@
+"use strict";
+var express 	= require('express');
+var bodyParser  = require('body-parser');
+var	module 		= require('./module/module');
+var app 		= express();
+var port 		= process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (request, response, next) {
+	response.header("Access-Control-Allow-Origin",  "*");
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, token, Accept");
+    response.header("Access-Control-Allow-Methods", "PUT");
+	next();
+});
+
+app.put('/board', module.handleBoard);
+
+app.listen(port);
+console.log('WORKSHOP 2 - TEAM BADGER - Service IA2 listening on port ' + port + '...');
